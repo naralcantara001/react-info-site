423e8b7857f3d172aebf0a80d1b4a0fecb8d0840
naralcantara001.nar <naralcantara001@gmail.com>
2022-07-12 14:29:29 +0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div>
+            <img src='images/react-logo.png' alt='logo'/>
+            <p>ReactFacts</p>
+        </div>
+        <p className='react-course'>React Course - Project 1</p>
+    </div>
+  )
+}
